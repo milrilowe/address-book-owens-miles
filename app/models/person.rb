08b2dc3ba25e73129ee3eb064dd247fa51314496
milrilowe <milesrhowens@gmail.com>
2022-12-29 12:50:27 -0700

    Added to SSN regex to also allow blank string so ssn is optional

--- app/models/person.rb
+++ app/models/person.rb
@@ -9,10 +9,10 @@ class Person < ApplicationRecord
 
   validates :last_name, presence: true
 
-
   # https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s12.html
+  # ^$ matches an empty string, ^(?!... matches an SSN.  There is a | between the two expressions because SSN is optional
   validates :ssn, format: {
-    with: /\A^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!000)[0-9]{4}$\z/,
-    message: "must follow XXX-XX-XXXX format"
+    with: /\A^$|^(?!000|666)[0-8][0-9]{2}-(?!00)[0-9]{2}-(?!000)[0-9]{4}$\z/,
+    message: "must follow XXX-XX-XXXX format and be a valid SSN"
   }
 end
