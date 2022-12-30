c7820a14ad5169f19210f05d21ad8fc9abdbee65
milrilowe <milesrhowens@gmail.com>
2022-12-30 01:37:32 -0700

    Refactor controllers to handle new route config (not working yet, though)

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,59 @@
 class AddressesController < ApplicationController
+  before_action :set_address, only: [:show, :edit, :update, :destroy]
+  before_action :set_person, only: [:show, :new, :create]
   def index
+    @addresses = Address.all
   end
+
+  def show
+
+  end
+
+  def new
+    @address = Address.new
+  end
+
+  def create
+    @address = Address.new(address_params)
+
+    if @address.save
+      redirect_to person_path(@person)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+    @address.update(address_params)
+
+    if @address.update
+      redirect_to @address
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @address.destroy
+
+    redirect_to addresses_path
+  end
+
+  private
+    def address_params
+      params.require(:address).permit(:street, :town, :zip_code, :state,  :country, :person_id)
+    end
+
+    def set_address
+      @address = Address.find(params[:id])
+    end
+
+    def set_person
+      @person = Person.find(params[:person_id])
+    end
+
 end
