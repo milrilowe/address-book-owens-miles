daf1f1284ae55325a0b680a0fed01eec9c939797
milrilowe <milesrhowens@gmail.com>
2022-12-30 01:35:23 -0700

    Added a table of countries for address form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_29_223710) do
+ActiveRecord::Schema.define(version: 2022_12_30_072002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2022_12_29_223710) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "person_id", null: false
     t.index ["person_id"], name: "index_addresses_on_person_id"
+  end
+
+  create_table "countries", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "emails", force: :cascade do |t|
