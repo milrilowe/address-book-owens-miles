f6a309b1a9becb444dea6b2a74b955a4cb42cc25
milrilowe <milesrhowens@gmail.com>
2022-12-31 15:34:30 -0700

    Config bootstrap/jquery/popperjs

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,11 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+
+require("@popperjs/core")
+require('jquery')
+require("../stylesheets/application.scss")
 
 Rails.start()
 Turbolinks.start()
