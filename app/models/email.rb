ef92fe75d3113403721ed1dfc9a75568d01e2152
milrilowe <milesrhowens@gmail.com>
2023-01-02 07:25:45 -0700

    Added email validation with regex

--- app/models/email.rb
+++ app/models/email.rb
@@ -1,3 +1,6 @@
 class Email < ApplicationRecord
   belongs_to :person
+
+  validates :email_address, presence: true
+  validates :email_address, format: { with: /\A(^[a-zA-Z0-9_.+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-.]+$)\z/, message: "must be a valid email address" }
 end
