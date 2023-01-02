ce7fa0afa4236d973566d9ab8654fda2754817e8
milrilowe <milesrhowens@gmail.com>
2023-01-02 02:06:21 -0700

    Changes just reflect css id name changes

--- app/javascript/packs/fadeToggle.js
+++ app/javascript/packs/fadeToggle.js
@@ -1,16 +1,16 @@
 $(document).on('turbolinks:load', function() {
   console.log('fadeToggle.js loaded')
-  $('#addresses-link').click(function() {
+  $('#addresses-heading').click(function() {
     $('#addresses-section').fadeToggle();
     return false; // prevent page reload
   });
 
-  $('#emails-link').click(function() {
+  $('#emails-heading').click(function() {
     $('#emails-section').fadeToggle();
     return false; // prevent page reload
   });
 
-  $('#phone-numbers-link').click(function() {
+  $('#phone-numbers-heading').click(function() {
     $('#phone-numbers-section').fadeToggle();
     return false; // prevent page reload
   });
