5a462e1adfe87e28cac32a605b3c8846187fc4f2
milrilowe <milesrhowens@gmail.com>
2023-01-05 14:07:58 -0700

    Added set user before action and added an authenticate action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,16 @@
 class ApplicationController < ActionController::Base
-  # protect_from_forgery with: :null_session
+  protect_from_forgery with: :null_session
+
+  before_action :set_current_user
+
+  def set_current_user
+    if session[:user_id]
+      Current.user = User.find_by(id: session[:user_id])
+    end
+  end
+
+  def authenticate
+    redirect_to root_path, notice: "You must be logged in." unless Current.user
+  end
+
 end
