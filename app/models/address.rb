d4e16a0e2a6cd7f58c0e4bddd1963932033979a5
milrilowe <milesrhowens@gmail.com>
2023-01-05 15:40:37 -0700

    Added street, town, zip_code as required fields

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,11 @@
 class Address < ApplicationRecord
   belongs_to :person
+
+  validates :street, presence: true
+  validates :street, length: { maximum: 30 }
+
+  validates :city, presence: true
+  validates :city, length: { maximum: 15 }
+
+  validates :state, presence: true
 end
