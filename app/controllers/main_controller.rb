55306eef14f529a783903d5c7d2408a29b46d871
milrilowe <milesrhowens@gmail.com>
2023-01-05 14:15:51 -0700

    Added a home page controller

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,10 @@
+class MainController < ApplicationController
+  def index
+    if session[:user_id]
+      @user = User.find_by(id: session[:user_id])
+      @people = @user.people
+    else
+      @user = User.new
+    end
+  end
+end
