2ed3809290dc1b8ccc3c167e3166fe4a88953354
milrilowe <milesrhowens@gmail.com>
2023-01-05 14:06:05 -0700

    Added authentication and destroy logic to keep track of prev address

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,4 +1,6 @@
 class AddressesController < ApplicationController
+  before_action :authenticate
+
   before_action :set_person
   before_action :set_address, only: [:show, :update, :destroy, :edit]
 
@@ -28,12 +30,12 @@ class AddressesController < ApplicationController
       if @address.save
         format.html { redirect_to person_path(@person), notice: "Address was successfully created." }
         format.json { render :show, status: :created, location: @person }
+        format.js # render create.js.erb
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @address.errors, status: :unprocessable_entity }
       end
     end
-
   end
 
   def edit
@@ -43,10 +45,13 @@ class AddressesController < ApplicationController
   end
 
   def update
+    @addresses = @person.addresses
+
     respond_to do |format|
       if @address.update(address_params)
         format.html { redirect_to @person, notice: "Address was successfully updated." }
         format.json { render :show, status: :ok, location: @person }
+        format.js # render update.js.erb
       else
         format.html { render :edit, status: :unprocessable_entity }
         format.json { render json: @address.errors, status: :unprocessable_entity }
@@ -55,10 +60,25 @@ class AddressesController < ApplicationController
   end
 
   def destroy
+    id = @address.id
     @address.destroy
+
+    prev_id = nil
+    @person.addresses.each do |address|
+      break if address.id > id
+      prev_id = address.id
+    end
+
+    if prev_id != nil
+      @address = @person.addresses.find(prev_id)
+    else
+      @address = @person.addresses.first
+    end
+
     respond_to do |format|
       format.html { redirect_to @person, notice: "Address was successfully destroyed.", status: :see_other }
       format.json { head json: @address.errors }
+      format.js # render destroy.js.erb
     end
   end
 
