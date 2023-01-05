822ed8a4ac7b9aa9219ae206854fad1b10ac765a
milrilowe <milesrhowens@gmail.com>
2023-01-05 14:13:51 -0700

    Added registration and session routes and set a main index as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  root "people#index"
+  root "main#index"
+
+  get "sign_up", to: "users#new"
+  post "sign_up", to: "users#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
 
   resources :people do
     resources :addresses, :emails, :phone_numbers
