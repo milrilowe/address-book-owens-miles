aacd40da68de730aeda86914e33716a6f20db24c
milrilowe <milesrhowens@gmail.com>
2023-01-05 15:37:00 -0700

    Added phone number regex validation

--- app/models/phone_number.rb
+++ app/models/phone_number.rb
@@ -1,3 +1,7 @@
 class PhoneNumber < ApplicationRecord
   belongs_to :person
+
+  validates :phone_number, presence: true
+  # https://stackoverflow.com/a/50122731
+  validates :phone_number, format: { with: /\A(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4})(\s?(([E|e]xt[:|.|]?)|x|X)(\s?\d+))?\z/, message: "Must be in a valid format" }
 end
