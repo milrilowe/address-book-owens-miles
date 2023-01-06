f965538e15a91246d3c67a8614b0d1ada12cad6e
milrilowe <milesrhowens@gmail.com>
2023-01-05 19:45:04 -0700

    Added api route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+
+  namespace :api do
+    namespace :v1 do
+      resources :people, only: [:index, :show, :create, :update, :destroy] do
+        resources :addresses, :emails, :phone_numbers
+      end
+    end
+  end
+
   root "main#index"
 
   get "sign_up", to: "users#new"
