080e1d85699c7e20505d9d9ecfc60de23806b7f3
milrilowe <milesrhowens@gmail.com>
2023-01-05 18:32:03 -0700

    Removed accidental commented out code

--- test/system/emails_test.rb
+++ test/system/emails_test.rb
@@ -35,38 +35,38 @@ class PeopleTest < ApplicationSystemTestCase
     assert_text(:all, "Comment: This is a comment.")
   end
 
-  # test "update an email" do
-  #   click_on "Last, First M."
+  test "update an email" do
+    click_on "Last, First M."
 
-  #   click_on "Emails"
-  #   within ".edit-email" do
-  #     click_on "Edit"
-  #   end
+    click_on "Emails"
+    within ".edit-email" do
+      click_on "Edit"
+    end
 
-  #   fill_in "Email address", with: ""
-  #   fill_in "Email address", with: "ema@il.com"
-  #   fill_in "Comment", with: ""
-  #   fill_in "Comment", with: "This is an updated comment."
+    fill_in "Email address", with: ""
+    fill_in "Email address", with: "ema@il.com"
+    fill_in "Comment", with: ""
+    fill_in "Comment", with: "This is an updated comment."
 
-  #   click_on "Update Email"
+    click_on "Update Email"
 
-  #   assert_text(:all, "ema@il.com")
-  #   assert_text(:all, "Comment: This is an updated comment.")
-  # end
+    assert_text(:all, "ema@il.com")
+    assert_text(:all, "Comment: This is an updated comment.")
+  end
 
-  # test "destroy an email" do
-  #   click_on "Last, First M."
+  test "destroy an email" do
+    click_on "Last, First M."
 
-  #   click_on "Emails"
-  #   within ".edit-email" do
-  #     click_on "Edit"
-  #   end
+    click_on "Emails"
+    within ".edit-email" do
+      click_on "Edit"
+    end
 
-  #   page.accept_confirm do
-  #     click_on "Delete"
-  #   end
+    page.accept_confirm do
+      click_on "Delete"
+    end
 
-  #   assert_text "No emails..."
-  #   assert_selector "h1", text: "Mr. First Middle Last"
-  # end
+    assert_text "No emails..."
+    assert_selector "h1", text: "Mr. First Middle Last"
+  end
 end
